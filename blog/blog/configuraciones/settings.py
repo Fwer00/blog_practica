3dfb6fcec3390eb760f057d474eb48953edc77d3
Fwer00 <129996733+Fwer00@users.noreply.github.com>
2023-09-14 03:22:10 -0300

    base de datos

--- blog/blog/configuraciones/settings.py
+++ blog/blog/configuraciones/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.posts',
 ]
 
 MIDDLEWARE = [
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'basesita',
+        'USER': 'Fwer',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
